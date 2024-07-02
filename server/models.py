97ffc38dfbcbe45deb2a61db0d9e3f147f76a306
student <kmac.kimaru93@gmail.com>
2024-07-02 23:46:28 +0300

    flask sqlalchemy validations lab

--- server/models.py
+++ server/models.py
@@ -7,11 +7,25 @@ class Author(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
-    phone_number = db.Column(db.String)
+    phone_number = db.Column(db.String(10), nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+      if not name:
+          raise ValueError('author name is required')
+      return name
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if not len(phone_number) == 10 or not phone_number.isdigit():
+            raise ValueError('phone_number must be 10 digits long')
+        return phone_number
+        
+            
+        
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,7 +42,32 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError('content must be at least 250 characters')
+        return content
+    
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError('summary must be a maximum of 250 characters')
+        return summary
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ["Fiction", "Non-Fiction"]:
+            raise ValueError('category must either be "Fiction" or "Non-Fiction"')
+        return category
 
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in title for phrase in clickbait_phrases):
+            raise ValueError('title is not a clickbait phrase')
+        return title
+        
+            
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
